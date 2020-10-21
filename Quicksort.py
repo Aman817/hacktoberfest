8415b0d673321f74940cf5b4e300271ac5e80ee9
Aman817 <51746589+Aman817@users.noreply.github.com>
2020-10-21 14:21:24 +0530

    Create Quicksort.py

---
+++ Quicksort.py
@@ -0,0 +1,40 @@
+# Python program for implementation of Quicksort Sort 
+  
+
+def partition(arr,low,high): 
+    i = ( low-1 )         
+    pivot = arr[high]     
+  
+    for j in range(low , high): 
+  
+       
+        if   arr[j] < pivot: 
+          
+           
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+  
+
+  
+
+def quickSort(arr,low,high): 
+    if low < high: 
+  
+        
+        pi = partition(arr,low,high) 
+  
+        
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
+  
+ 
+arr = [67,10,45,78,90,9,56,8,34,689,345] 
+n = len(arr) 
+quickSort(arr,0,n-1) 
+print ("Sorted array :") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
+  
